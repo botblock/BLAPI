c18956c878e16c5ab983d6447a8ceb146939de22
Jonah Snider <me@jonahsnider.ninja>
2018-09-05 20:42:15 -0700

    refactor: Use ESLint and make code style uniform

--- bttps.js
+++ bttps.js
@@ -1,45 +1,43 @@
 const https = require('https');
 
 module.exports = {
-    //custom made post function
-    post: async (domain, apiPath, apiKey, sendObj) => {
-        let postData = JSON.stringify(sendObj);
-        let options = {
-            hostname: domain,
-            port: 443,
-            path: apiPath,
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Content-Length': postData.length,
-                'Authorization': apiKey
-            }
-        };
-        let req = https.request(options, () => { });
-        req.on('error', (e) => {
-            console.error(e);
-        });
-        req.write(postData);
-        req.end();
-    },
-    //custom made get function
-    get: async (url) => {
-        return new Promise((resolve, reject) => {
-            https.get(url, (resp) => {
-                let data = '';
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-                resp.on('end', () => {
-                    resolve(JSON.parse(data));
-                });
-                resp.on('error', (e) => {
-                    console.error(e);
-                    reject(`Request to ${resp.url} failed with HTTP ${resp.status}`);
-                });
-            })
-        })
-    }
-}
+  // custom made post function
+  post: (domain, apiPath, apiKey, sendObj) => {
+    const postData = JSON.stringify(sendObj);
+    const options = {
+      hostname: domain,
+      port: 443,
+      path: apiPath,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length,
+        'Authorization': apiKey
+      }
+    };
+    const req = https.request(options, () => { });
+    req.on('error', e => {
+      console.error(e);
+    });
+    req.write(postData);
+    req.end();
+  },
+  // custom made get function
+  get: url => new Promise((resolve, reject) => {
+    https.get(url, resp => {
+      let data = '';
+      resp.on('data', chunk => {
+        data += chunk;
+      });
+      resp.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+      resp.on('error', e => {
+        console.error(e);
+        reject(new Error(`Request to ${resp.url} failed with HTTP ${resp.status}`));
+      });
+    });
+  })
+};
 
 
