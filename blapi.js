33c15390fa4419c1252bb985d40393f5579052cf
Sebastian Di Luzio <sebidiluzio@gmail.com>
2019-07-15 10:32:05 +0200

    first changes, completely untested

--- blapi.js
+++ blapi.js
@@ -8,17 +8,24 @@ let extendedLogging = false;
 let useBotblockAPI = true;
 
 /**
- * @param {object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.} ; it also includes other metadata including sharddata
+ * @param {Object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.} ; it also includes other metadata including sharddata
  */
 const postToAllLists = async apiKeys => {
   // make sure we have all lists we can post to and their apis
   const currentDate = new Date();
   if (!listData || listAge < currentDate) {
     listAge.setDate(currentDate.getDate() + 1); // we try to update the listdata every day, in case new lists are added but the code is not restarted
-    listData = await bttps.get('https://botblock.org/api/lists').catch(e => console.error(`BLAPI: ${e}`));
-    if (!listData) {
-      console.error("BLAPI : Something went wrong when contacting BotBlock for the API of the lists, so we're using an older preset. Some lists might not be available because of this.");
-      listData = fallbackListData;
+    try {
+      const tmpListData = await bttps.get('https://botblock.org/api/lists');
+      // make sure we only save it if nothing goes wrong
+      if (tmpListData) {
+        listData = tmpListData;
+      } else {
+        throw new Error('Got empty list from botblock.');
+      }
+    } catch (e) {
+      console.error(`BLAPI: ${e}`);
+      console.error('BLAPI : Something went wrong when contacting BotBlock for the API of the lists, so we\'re using an older preset. Some lists might not be available because of this.');
     }
   }
   for (const listname in listData) {
@@ -27,10 +34,9 @@ const postToAllLists = async apiKeys => {
       if (listname === 'discordbots.org') {
         list = fallbackListData[listname];
       }
-      const url = `https://${listname}`;
-      const apiPath = list.api_post.replace(url, '').replace(':id', apiKeys.bot_id);
+      const apiPath = list.api_post.replace(':id', apiKeys.bot_id);
       // creating JSON object to send, reading out shard data
-      let sendObj = {};
+      const sendObj = {};
       sendObj[list.api_field] = apiKeys.server_count;
       if (apiKeys.shard_id && list.api_shard_id) {
         sendObj[list.api_shard_id] = apiKeys.shard_id;
@@ -41,14 +47,14 @@ const postToAllLists = async apiKeys => {
       if (apiKeys.shards && list.api_shards) {
         sendObj[list.api_shards] = apiKeys.shards;
       }
-      bttps.post(listname, apiPath, apiKeys[listname], sendObj, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
+      bttps.post(apiPath, apiKeys[listname], sendObj, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
     }
   }
 };
 
 /**
  * @param {Client} client Discord.js client
- * @param {object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
+ * @param {Object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
  * @param {number} repeatInterval Number of minutes between each repetition
  */
 const handleInternal = async (client, apiKeys, repeatInterval) => {
@@ -64,7 +70,7 @@ const handleInternal = async (client, apiKeys, repeatInterval) => {
       apiKeys.shard_count = client.shard.count;
 
       // This will get as much info as it can, without erroring
-      const _ = await client.shard.broadcastEval('this.guilds.size').catch(e => console.error('BLAPI: Error while fetching shard server counts:', e)), shardCounts = _.filter(count => count !== 0);
+      const _ = await client.shard.broadcastEval('this.guilds.size').catch(e => console.error('BLAPI: Error while fetching shard server counts:', e)); const shardCounts = _.filter(count => count !== 0);
       if (shardCounts.length !== client.shard.count) {
         // If not all shards are up yet, we skip this run of handleInternal
         return;
@@ -95,11 +101,11 @@ const handleInternal = async (client, apiKeys, repeatInterval) => {
       // Check if bot is not sharded at all, but still wants to send server count (it's recommended to shard your bot, even if it's only one shard)
     } else if (!client.shard) {
       apiKeys.server_count = client.guilds.size;
-    } else unchanged = true; // nothing has changed, therefore we don't send any data
+    } else { unchanged = true; } // nothing has changed, therefore we don't send any data
     /* eslint-enable camelcase */
     if (!unchanged) {
       if (repeatInterval > 2 && useBotblockAPI) { // if the interval isnt below the BotBlock ratelimit, use their API
-        bttps.post('botblock.org', '/api/count', 'no key needed for this', apiKeys, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
+        bttps.post('https://botblock.org/api/count', 'no key needed for this', apiKeys, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
 
         // they blacklisted botblock, so we need to do this, posting their stats manually
         if (apiKeys['discordbots.org']) {
@@ -130,7 +136,7 @@ module.exports = {
   /**
    * This function is for automated use with discord.js
    * @param {Client} discordClient Client via wich your code is connected to Discord
-   * @param {object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
+   * @param {Object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
    * @param {number} repeatInterval Number of minutes until you want to post again, leave out to use 30
    */
   handle: (discordClient, apiKeys, repeatInterval) => {
@@ -142,7 +148,7 @@ module.exports = {
    * For when you don't use discord.js or just want to post to manual times
    * @param {number} guildCount Integer value of guilds your bot is serving
    * @param {string} botID Snowflake of the ID the user your bot is using
-   * @param {object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
+   * @param {Object} apiKeys A JSON object formatted like: {"botlist name":"API Keys for that list", etc.}
    * @param {number} shardID (optional) The shard ID, which will be used to identify the shards valid for posting (and for super efficient posting with BLAPIs own distributer when not using botBlock)
    * @param {number} shardCount (optional) The number of shards the bot has, which is posted to the lists
    * @param {number[]} shards (optional) An array of guild counts of each single shard (this should be a complete list, and only a single shard will post it)
@@ -166,7 +172,7 @@ module.exports = {
       /* eslint-enable camelcase */
     }
     if (useBotblockAPI) {
-      bttps.post('botblock.org', '/api/count', 'no key needed for this', apiKeys, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
+      bttps.post('https://botblock.org/api/count', 'no key needed for this', apiKeys, extendedLogging).catch(e => console.error(`BLAPI: ${e}`));
     } else {
       postToAllLists(apiKeys);
     }
