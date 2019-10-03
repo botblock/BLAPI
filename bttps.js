fb80e5fcf322fce578b35509d007370f4258ca1f
Gleeny <killdore@outlook.com>
2019-10-03 23:46:06 +0200

    Make a completely new system using node-fetch
    I got issues with the https-package, saying `"listener" argument must be a function` and `getaddrinfo ENOTFOUND https://botblock.org/api/count https://botblock.org/api/count:443`.

--- bttps.js
+++ bttps.js
@@ -1,9 +1,35 @@
+const fetch = require("node-fetch")
 
 module.exports = {
   // custom made post function
   post: (apiPath, apiKey, sendObj, logStuff) => new Promise((resolve, reject) => {
+    const postData = JSON.stringify(sendObj);
+
+    fetch(apiPath, {
+      method: 'POST',
+      body: postData,
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length,
+        'Authorization': apiKey
+      }
+    }).then(res => {
+      if (logStuff) {
+        console.log('BLAPI: posted to', apiPath);
+        console.log('BLAPI: statusCode:', res.status, res.statusText);
+        console.log('BLAPI: headers:', res.headers.raw())
+      }
+      return res.text(); // it's text because text accepts both json and plain text, while json only supports json
+    }).then(body => resolve(console.log('BLAPI: data:', body))).catch(e => {
+      console.error('BLAPI:', e);
+      reject(new Error(`Request to ${req.url} failed with Errorcode ${req.status}:\n${req.statusText}`));
+    })
   }),
   // custom made get function
   get: url => new Promise((resolve, reject) => {
+    fetch(url).then(res => res.json()).then(resolve).catch(e => {
+      console.error('BLAPI:', e);
+      reject(new Error(`Request to ${resp.url} failed with Errorcode ${resp.status}:\n${resp.statusText}`));
+    })
   })
 };
