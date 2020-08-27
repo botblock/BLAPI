42e3c5501d8120cbd780fa95d8dc88484e4d34e5
Anish <anishanne1@gmail.com>
2020-08-27 11:05:35 -0400

    Add bots.discordlabs.org (#58)
    * Add bots.discordlabs.org
    
    * Added all bot lists.
    
    * Removed object in object & replaced double quotes with single quotes.
    
    * Fixed code errors & reorganized lists.
    
    * Update legacyIdsFallbackData.ts
    
    Co-authored-by: Sebastian Di Luzio <sebidiluzio@gmail.com>

--- src/legacyIdsFallbackData.ts
+++ src/legacyIdsFallbackData.ts
@@ -1,5 +1,9 @@
-
 export default {
+  'vultrex.io': 'discordbots.co',
+  'space-bot-list.org': 'space-bot-list.xyz',
+  'discordlist.co': 'nooder.co',
   'discordbots.org': 'top.gg',
+  'discordbotreviews.xyz': 'nooder.co',
   'botsparadiscord.xyz': 'botsparadiscord.com',
+  'arcane-botcenter.xyz': 'arcane-center.xyz',
 };
