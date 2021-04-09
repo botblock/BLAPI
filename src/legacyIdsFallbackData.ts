5484e8ab809052a64b562f3bea08b590bca50b70
Sebastian Di Luzio <sebidiluzio@gmail.com>
2021-04-09 16:47:23 +0200

    update fallback data
    pulled 9-4-2021@16:40

--- src/legacyIdsFallbackData.ts
+++ src/legacyIdsFallbackData.ts
@@ -1,9 +1,14 @@
 export default {
   'vultrex.io': 'discordbots.co',
   'space-bot-list.org': 'space-bot-list.xyz',
+  'mythicalbots.xyz': 'bots.idledev.org',
+  'infinitybots.xyz': 'infinitybotlist.com',
+  'infinitybots.com': 'infinitybotlist.com',
+  'distop.xyz': 'bots.distop.xyz',
   'discordlist.co': 'nooder.co',
   'discordbots.org': 'top.gg',
   'discordbotreviews.xyz': 'nooder.co',
   'botsparadiscord.xyz': 'botsparadiscord.com',
+  'botlist.space': 'discordlist.space',
   'arcane-botcenter.xyz': 'arcane-center.xyz',
 };
